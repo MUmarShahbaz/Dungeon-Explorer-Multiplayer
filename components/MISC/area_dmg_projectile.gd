e9054204246bc52c9e8c9f8ecf549efcbe9acf25
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2026-02-21 04:27:23 +0500

    Made a area damage projectile

---
+++ components/MISC/area_dmg_projectile.gd
@@ -0,0 +1,18 @@
+extends Projectile
+class_name AreaDamageProjectile
+
+@export var blast_radius : Area2D
+
+func _ready() -> void:
+	super._ready()
+	blast_radius.set_collision_mask_value(3 if launched_by is Player else 2, true)
+	blast_radius.set_collision_mask_value(5, true)
+
+func _physics_process(_delta: float) -> void:
+	var bodies = get_colliding_bodies()
+	if bodies.size() > 0:
+		for body in blast_radius.get_overlapping_bodies():
+			if body is Entity:
+				(body as Entity).take_damage(damage)
+				(body as Entity).velocity.x += 10*direction
+		queue_free()
