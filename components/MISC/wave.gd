e8ff5abc92e15f7c8229d37bf1569958e93ed297
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2026-03-14 22:33:41 +0500

    Major updates to the map and created hellhounds

--- components/MISC/wave.gd
+++ components/MISC/wave.gd
@@ -3,7 +3,7 @@ class_name WaveOfMonsters
 
 signal wave_finished
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if get_child_count() == 0:
 		wave_finished.emit()
 		queue_free()
