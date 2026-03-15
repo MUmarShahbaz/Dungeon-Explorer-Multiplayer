22fa8c64963150c34d5dbeeb86446fdab97ae6db
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2026-03-16 03:35:20 +0500

    Isolated Map and Lvls Allows reusability of maps

--- assets/tilemap/traps.gd
+++ assets/tilemap/traps.gd
@@ -16,18 +16,18 @@ func _ready() -> void:
 	for cell in get_used_cells():
 		match get_cell_atlas_coords(cell):
 			cells.breakable_floors:
-				create_area(cell, Vector2(0, -36), "break_floor")
+				create_area(cell, Vector2(0, -18), Vector2(48, 48), "break_floor")
 			cells.spikes_in:
-				create_area(cell, Vector2(0, 12), "activate_spike")
+				create_area(cell, Vector2(0, 12), Vector2(48, 24), "activate_spike")
 			cells.spikes_out:
-				create_area(cell, Vector2(0, 12), "activate_spike")
+				create_area(cell, Vector2(0, 12), Vector2(48, 24), "activate_spike")
 
-func create_area(cell, local_position, handler):
+func create_area(cell, local_position, size, handler):
 	var shape    : RectangleShape2D = RectangleShape2D.new()
 	var collider : CollisionShape2D = CollisionShape2D.new()
 	var area     : Area2D           = Area2D.new()
 	
-	shape.size = Vector2(48, 24)
+	shape.size = size
 	collider.shape = shape
 	area.add_child(collider)
 	area.global_position = map_to_local(cell) + local_position
--- scenes/player_spawner/player_spawner.gd
+++ scenes/player_spawner/player_spawner.gd
@@ -4,42 +4,29 @@ class_name PlayerSpawner
 var player_selector : PackedScene = preload("res://scenes/player_spawner/player_selector.tscn")
 var hud : PackedScene = preload("res://scenes/player_spawner/hud.tscn")
 
-signal player_spawned(player : Player)
+signal player_spawned(player : Player, death_count : int)
+var death_count := 0
 
-@rpc("authority", "call_remote", "reliable")
 func display_player_selector():
 	var new_selector = player_selector.instantiate()
 	(new_selector.continue_btn as Button).pressed.connect(func ():
 		if new_selector.selected:
-			spawn.rpc(new_selector.selected, multiplayer.get_unique_id())
+			spawn(new_selector.selected, true)
 			new_selector.queue_free()
 	)
 	get_tree().get_current_scene().add_child.call_deferred(new_selector)
 
-@rpc("any_peer", "call_local", "reliable")
-func spawn(selected, id):
-	if !multiplayer.is_server(): return
+func spawn(selected, first_spawn = false):
+	if not first_spawn: death_count += 1
 	var new_player = load(selected[&"scene"]).instantiate()
-	new_player.name = str(id)
-	(new_player as Player).entity_died.connect(spawn.rpc.bind(selected, id))
+	(new_player as Player).entity_died.connect(spawn.bind(selected))
+	(new_player as Player).global_position = self.global_position
 	get_tree().current_scene.add_child.call_deferred(new_player)
-	await new_player.ready
-	await get_tree().physics_frame
-	position_rpc.rpc(id, global_position)
-	player_spawned.emit(new_player)
-	if id != 1: create_hud.rpc_id(id, id, selected[&"image"])
-	else: create_hud(id, selected[&"image"])
-
-@rpc("any_peer", "call_local", "reliable")
-func position_rpc(id : int, new_position: Vector2):
-	get_tree().current_scene.get_node_or_null(str(id)).global_position = new_position
-
-@rpc("reliable")
-func create_hud(id, image):
 	var new_hud = hud.instantiate()
-	new_hud.multiplayer_id = id
+	new_hud.player = new_player
 	new_hud.avatar = AtlasTexture.new()
-	new_hud.avatar.atlas = load(image)
+	new_hud.avatar.atlas = load(selected[&"image"])
 	new_hud.avatar.region.position = Vector2(16, 8)
 	new_hud.avatar.region.size = Vector2(32, 32)
 	add_child.call_deferred(new_hud)
+	player_spawned.emit(new_player, death_count)
